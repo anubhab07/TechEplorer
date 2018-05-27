1ab56e7b1feaa4850838894df0a8f8a29461538a
anubhab07 <34179359+anubhab07@users.noreply.github.com>
2018-05-27 20:46:10 +0530

    starter web page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { DashboardRoutingModule } from './app.route';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent, LoginDialogComponent } from './login/login.component';
+import { TechListComponent } from './tech-list/tech-list.component';
+
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    LoginDialogComponent,
+    TechListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    DashboardRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    NgbModule.forRoot()
+  ],
+  entryComponents: [
+    LoginDialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
