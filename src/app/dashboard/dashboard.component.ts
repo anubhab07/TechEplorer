f31a9d97a32080c6c7c1b9b8fd5bfb11145e2ef2
Nilesh Sinha <nileshinit@gmail.com>
2018-06-03 23:08:01 +0530

    broken dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import {DataService} from '../data.service';
 import {WebCallingService} from '../web-calling.service';
 
@@ -7,12 +7,14 @@ import {WebCallingService} from '../web-calling.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements DoCheck {
   searchTerm = '';
   constructor(private _dataService: DataService, private _webService: WebCallingService) { }
   eventsList = [];
-  ngOnInit() {
-    this.eventsList = this._dataService.eventsList;
+  ngDoCheck() {
+    if(this.eventsList != this._dataService.eventsList) {
+      this.eventsList = this._dataService.eventsList;
+    }
   }
   submitSearch() {
     console.log(this.searchTerm);
